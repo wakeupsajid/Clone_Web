4df11ce94eacb10f83c1e3b356da28e73504bee5
Wakeupsajid <sajidali222138@gmail.com>
2026-08-18 12:05:25 +0500

    Connect frontend to public backend

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -14,7 +14,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://sage-parking-weekly-reason.trycloudflare.com/api/auth/login",
         {
           email,
           password,
